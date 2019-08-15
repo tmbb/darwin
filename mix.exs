23d9ff2fe6f76cf80c8535c1544bc4a589f2fe65
tmbb <tmbb@campus.ul.pt>
2019-08-15 23:00:02 +0100

    move ex_to_erl into darwin

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Darwin.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:benchee, "~> 1.0.1"},
+      {:benchee, "~> 1.0.1", only: [:dev]},
       {:stream_data, "~> 0.4.3", only: [:dev, :test]},
       {:ex_doc, github: "elixir-lang/ex_doc", only: [:dev, :doc]},
       {:assert_value, "~> 0.9.2", only: [:dev, :test]}
